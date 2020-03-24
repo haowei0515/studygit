e2cb3413135c2c6b537f8c43c32da3df4d68f5e6
haowei03 <haowei03@baidu.com>
2020-03-24 19:22:54 +0800

    测试travis

---
+++ hello_print.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+################################################################################
+#
+# Copyright (c) 2019 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+
+class PutTemplate(object):
+    "用于vpc异常测试"
+    def put_template(self):
+	print "hello"
+
+
+if __name__ == '__main__':
+    PutTemplate().put_template()
